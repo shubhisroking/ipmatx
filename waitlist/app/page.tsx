eb71837fcee37d079f0705d5a3e65fc8c18a64d1
Shubh <shubhisroking@proton.me>
2025-05-06 23:56:24 +0530

    coming soon
    Signed-off-by: Shubh <shubhisroking@proton.me>

---
+++ waitlist/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <h1 className="text-4xl font-bold text-center">Coming Soon</h1>
+    </div>
+  );
+}
